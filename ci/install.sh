14d836cbd4623d67e0a28621e99dc2b9fe2c3ed4
elbaro <elbaro@github>
2018-04-28 18:30:33 +0900

    travis

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,8 @@ set -ex
 
 main() {
     local target=
-    if [ $TRAVIS_OS_NAME = linux ]; then
+    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	sudo apt-get update
         sudo apt-get install -y libvips-dev
 
         target=x86_64-unknown-linux-musl
@@ -16,7 +17,7 @@ main() {
 
     # Builds for iOS are done on OSX, but require the specific target to be
     # installed.
-    case $TARGET in
+    case "$TARGET" in
         aarch64-apple-ios)
             rustup target install aarch64-apple-ios
             ;;
