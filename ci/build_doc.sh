009ec932de940e33070471b95c5ca4b1eedeec6f
elbaro <elbaro@github>
2018-04-28 18:50:15 +0900

    travis

--- ci/build_doc.sh
+++
@@ -1,4 +0,0 @@
-[ $TRAVIS_BRANCH = master ] &&
-[ $TRAVIS_PULL_REQUEST = false ] &&
-cargo doc &&
-echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html
